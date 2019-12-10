0f54399e43fbb75c40d7e1349053a602e13b95d7
carmi <carmi@DESKTOP-G6FFQOM>
2019-12-10 16:01:13 +0100

    Abbellimento e completamento prima finestra

--- src/PannelloPrimario.java
+++ src/PannelloPrimario.java
@@ -8,32 +8,24 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class PannelloPrimario extends JFrame {
 
 	private JPanel contentPane;
 	FinestraPrimariaController controllerPannello = new FinestraPrimariaController();
-	private JTextField campoDaRiempire;
-	
-	/**
-	 * Launch the application.
-	 *//*
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PannelloPrimario frame = new PannelloPrimario();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}*/
 
-	/**
-	 * Create the frame.
-	 */
+	private JTextField nomeTextField;
+	private JTextField cognomeTextField;
+	private JTextField matricolaTextField;
+
+	private JButton bottoneStampa;
+	private JButton bottoneSalva;
+	
+	
+	
+	
 	public PannelloPrimario(FinestraPrimariaController controllerPannelloLocale) {
 		controllerPannello = controllerPannelloLocale;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -43,20 +35,51 @@ public class PannelloPrimario extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton BottoneFighissimo = new JButton("Premi ");
-		BottoneFighissimo.addActionListener(new ActionListener() {
+		bottoneSalva = new JButton("Salva");
+		bottoneSalva.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//System.out.println(campoDaRiempire.getText());
-				controllerPannello.creaStudente(campoDaRiempire.getText());
 				
 			}
 		});
-		BottoneFighissimo.setBounds(303, 210, 89, 23);
-		contentPane.add(BottoneFighissimo);
+		bottoneSalva.setBounds(303, 210, 89, 23);
+		contentPane.add(bottoneSalva);
 		
-		campoDaRiempire = new JTextField();
-		campoDaRiempire.setBounds(30, 30, 86, 20);
-		contentPane.add(campoDaRiempire);
-		campoDaRiempire.setColumns(10);
+		nomeTextField = new JTextField();
+		nomeTextField.setBounds(20, 35, 150, 20);
+		contentPane.add(nomeTextField);
+		nomeTextField.setColumns(10);
+		
+		bottoneStampa = new JButton("Stampa");
+		bottoneStampa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		bottoneStampa.setBounds(204, 210, 89, 23);
+		contentPane.add(bottoneStampa);
+		
+		cognomeTextField = new JTextField();
+		cognomeTextField.setColumns(10);
+		cognomeTextField.setBounds(20, 85, 150, 20);
+		contentPane.add(cognomeTextField);
+		
+		JLabel lblNome = new JLabel("Nome");
+		lblNome.setFont(new Font("Calibri", Font.PLAIN, 14));
+		lblNome.setBounds(20, 15, 40, 20);
+		contentPane.add(lblNome);
+		
+		JLabel lblCognome = new JLabel("Cognome");
+		lblCognome.setFont(new Font("Calibri", Font.PLAIN, 14));
+		lblCognome.setBounds(20, 70, 75, 15);
+		contentPane.add(lblCognome);
+		
+		JLabel lblMatricola = new JLabel("Matricola");
+		lblMatricola.setFont(new Font("Calibri", Font.PLAIN, 14));
+		lblMatricola.setBounds(20, 125, 70, 15);
+		contentPane.add(lblMatricola);
+		
+		matricolaTextField = new JTextField();
+		matricolaTextField.setBounds(20, 140, 150, 20);
+		contentPane.add(matricolaTextField);
+		matricolaTextField.setColumns(10);
 	}
 }
