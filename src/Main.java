13436cbf513c2b2c99d817c910f6eaf547967ca8
ubuntu01 <dimalozyak1997@gmail.com>
2019-12-10 18:20:10 +0100

    Revert "Pulizia Commenti inutili"
    This reverts commit b93639b17d6f308f546d05acbf537c8ae382b493.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		FinestraPrimariaController controller = new FinestraPrimariaController();
 		controller.initFinistra();
-		
+		//ciao sono fortissimo
+		//ciao sono sempre bellissimo
 	}
 	// prova cambio
 }
