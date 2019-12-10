b93639b17d6f308f546d05acbf537c8ae382b493
Carmine Testa <carmine.testa97@libero.it>
2019-12-10 18:11:49 +0100

    Pulizia Commenti inutili

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		FinestraPrimariaController controller = new FinestraPrimariaController();
 		controller.initFinistra();
-		//ciao sono fortissimo
-		//ciao sono sempre bellissimo
+		
 	}
 
 }
